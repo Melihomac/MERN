05e7593fd2bac28aa0ef09e88f37e35f78d31486
Melihomac <37351493+Melihomac@users.noreply.github.com>
2022-06-29 11:20:46 +0300

    Update

--- express.js
+++ express.js
@@ -143,3 +143,56 @@
 // app.listen(8081);
 
 //----------------
+
+//users.json dosyasındaki verileri listeleme işlemi.
+// var express = require('express');
+// var app = express();
+// var fs = require('fs');
+
+// app.get('/list_users', function(req, res){
+//   fs.readFile(__dirname + "/" + "users.json", function(err, data){
+//     if (err) throw err;
+//     console.log(data);
+//     res.end(data);
+//   });
+// });
+
+// var server = app.listen('8081', function(){
+//   var host = server.address().address;
+//   var port = server.address().port;
+//   console.log('http://%s:%s', host, port);
+// });
+
+//-----------------
+
+//users.join dosyasına veri ekleme işlemi
+// var express = require('express');
+// var app = express();
+// var fs = require("fs");
+
+// var user = {
+//    "user4" : {
+//       "name" : "Miros",
+//       "password" : "password4",
+//       "profession" : "teacher",
+//       "id": 4
+//    },
+// }
+
+// app.get('/add_user', function (req, res) {
+//    // First read existing users.
+//    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+//       data = JSON.parse( data );
+//       data["user4"] = user["user4"];
+//       console.log( data );
+//       res.end( JSON.stringify(data));
+//    });
+// })
+
+// var server = app.listen(8081, function () {
+//    var host = server.address().address
+//    var port = server.address().port
+//    console.log("Example app listening at http://%s:%s", host, port)
+// })
+
+//---------------
