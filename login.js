f15afee9d418b0029010a745f3120848e37e3a73
Melihomac <37351493+Melihomac@users.noreply.github.com>
2022-07-01 10:28:07 +0300

    Update

--- login.js
+++ login.js
@@ -1,24 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
-// router.get('/', function(req, res){
-//    res.send('GET route on things.');
-//    console.log('Login Screen');
-// });
-// router.post('/', function(req, res){
-//    res.send('POST route on things.');
-// });
+router.get('/login', function(req, res){
+  res.send(req.params);
+  console.log('Login Screen');
+});
 
-// module.exports = router;
-
-router.route('/:username/:id')
-    .get((req,res)=>{
-      validateID(req.params.id);
-    res.send(' welcome to page, ' + req.params.username)
-})
-    .post((req,res)=>{
-    processData(req.params.username);
-    res.send('data received ')
-})
+router.get('/login/:loginId', function(req, res){
+   res.send(req.params);
+   console.log('Login Screen');
+});
+router.post('/login', function(req, res){
+   res.send('POST route on things.');
+});
 
 module.exports = router;
+
+// router.route('/:username/:id')
+//     .get((req,res)=>{
+//       validateID(req.params.id);
+//     res.send(' welcome to page, ' + req.params.username)
+// })
+//     .post((req,res)=>{
+//     processData(req.params.username);
+//     res.send('data received ')
+// })
--- task.js
+++ task.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var app = express();
 
-
 var login = require('./login.js');
 
-app.use('/login', login);
+app.use('/', login);
 
 app.listen(3000);
